8e6ff8019a608e36ac8a9bfb35c362ed36dc82c5
Anthony Healy <w0407581@nscc.ca>
2020-02-05 14:22:40 -0400

    "up dated"

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <string.h>
+using namespace std;
+void openMaze(string url){
+    ifstream myFile;
+    myFile.open(url);
+    string line;
+    if(myFile.is_open()){
+        cout<<"opened"<<endl;
+        while(getline(myFile,line)){
+            cout<<line<<endl;
+        }
 
+
+    } else{
+        std::cout<<"failed to opened"<<std::endl;
+    }
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string maze_URL="..\\Maps\\maze.txt";
+    openMaze(maze_URL);
     return 0;
 }
+
+
