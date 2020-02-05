d445f93e6967d8a24330dd1e377664e0634fe6f0
Anthony Healy <w0407581@nscc.ca>
2020-02-05 09:20:32 -0400

    First

---
+++ cmake-build-debug/CMakeFiles/MazeFinder.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/School/DataStructure/AssignmentTwo/MazeFinder/MazeFinder/main.cpp" "E:/School/DataStructure/AssignmentTwo/MazeFinder/MazeFinder/cmake-build-debug/CMakeFiles/MazeFinder.dir/main.cpp.obj"
+  "E:/School/DataStructure/AssignmentTwo/MazeFinder/MazeFinder/tesr.cpp" "E:/School/DataStructure/AssignmentTwo/MazeFinder/MazeFinder/cmake-build-debug/CMakeFiles/MazeFinder.dir/tesr.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/MazeFinder.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MazeFinder.dir/main.cpp.obj"
+  "CMakeFiles/MazeFinder.dir/tesr.cpp.obj"
+  "MazeFinder.pdb"
+  "MazeFinder.exe"
+  "MazeFinder.exe.manifest"
+  "libMazeFinder.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MazeFinder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
